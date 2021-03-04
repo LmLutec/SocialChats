c65456cdce4d5bb2352566160c044c7071e3f20b
Lauren McCoy <lm_lutec@outlook.com>
2021-03-04 11:51:59 -0500

    added Firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDYMxmGReNZI1DeSKqHJ15uG7zljZND7Ds",
+    authDomain: "socialchats-88451.firebaseapp.com",
+    projectId: "socialchats-88451",
+    storageBucket: "socialchats-88451.appspot.com",
+    messagingSenderId: "461282623845",
+    appId: "1:461282623845:web:5caa7f4d81d7bb69f1218c",
+    measurementId: "G-56PDL59MMY"
+  };
+  // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
